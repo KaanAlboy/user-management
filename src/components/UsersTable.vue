fe0ce1fc9099b17fd1f6404aad273405e22577b6
Kaan Alboy <kaanalboy@gmail.com>
2024-09-30 22:23:07 +0300

    fix: table styles

--- src/components/UsersTable.vue
+++ src/components/UsersTable.vue
@@ -15,25 +15,25 @@ const emit = defineEmits(['sort-table', 'edit-button-click', 'delete-button-clic
       <thead>
         <tr>
           <th
-            class="px-4 py-3 text-left bg-slate-100 text-slate-500 font-normal border-solid rounded-tl-md hover:cursor-pointer"
+            class="px-4 py-3 text-left bg-slate-100 text-slate-500 font-normal border-solid rounded-tl-md hover:cursor-pointer select-none"
             @click="emit('sort-table', 'name')"
           >
             Name
           </th>
           <th
-            class="px-4 py-3 text-left bg-slate-100 text-slate-500 font-normal hover:cursor-pointer"
+            class="px-4 py-3 text-left bg-slate-100 text-slate-500 font-normal hover:cursor-pointer select-none"
             @click="emit('sort-table', 'email')"
           >
             Email
           </th>
           <th
-            class="px-4 py-3 text-left bg-slate-100 text-slate-500 font-normal hover:cursor-pointer"
+            class="px-4 py-3 text-left bg-slate-100 text-slate-500 font-normal hover:cursor-pointer select-none"
             @click="emit('sort-table', 'age')"
           >
             Age
           </th>
           <th
-            class="px-4 py-3 text-right bg-slate-100 text-slate-500 font-normal border-solid rounded-tr-md hover:cursor-pointer"
+            class="px-4 py-3 text-right bg-slate-100 text-slate-500 font-normal border-solid rounded-tr-md select-none"
           >
             Action
           </th>
